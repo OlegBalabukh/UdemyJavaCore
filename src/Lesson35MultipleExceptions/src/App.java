c836aeba7a1ccb9efc6e9947a75ca63b5024d02a
Oleg <olegukoff@gmail.comgit>
2018-08-20 23:02:53 +0300

    commit files in 'out' folder

---
+++ src/Lesson35MultipleExceptions/src/App.java
@@ -0,0 +1,75 @@
+package Lesson35MultipleExceptions.src;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+
+// 1
+/*
+public class App {
+    public static void main(String[] args) throws IOException, ParseException {
+        Test test = new Test();
+
+        test.run();
+    }
+}
+*/
+
+// 2
+
+public class App {
+    public static void main(String[] args) {
+        Test test = new Test();
+
+        ///////////// 1 //////////////
+        /*
+        try {
+            test.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            // e.printStackTrace();
+            System.out.println("Couldn't parse command file.");
+        }
+        */
+
+        ///////////// 2 //////////////
+        /*
+        try {
+            test.run();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        */
+
+        ///////////// 3 //////////////
+
+        // concept of polymorphism: you can always pass a child class wherever a parent class is expected
+        // Exception - parent class (for IOException ParseException) which catches all exceptions
+
+        try {
+            test.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            test.input();
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        *  FileNotFoundException before IOException (if vice-versa - will be an error) because
+        *  FileNotFoundException is a child of IOException and if parent IOException would be the fist,
+        *  it would be caught the exception and the next catch block with FileNotFoundException would be useless
+        *
+        *  catching  multiple exceptions, order them from more specific to more common
+        *
+        */
+
+
+    }
+}
